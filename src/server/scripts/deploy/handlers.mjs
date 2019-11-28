2510801de7a562f7500a93a862aeabe9437db92c
lanets <n.lanets@well-connect.net>
2019-11-29 01:26:06 +0300

    Fix generator. Dev

--- src/server/scripts/deploy/handlers.mjs
+++ src/server/scripts/deploy/handlers.mjs
@@ -1,56 +1,73 @@
 
-import ora from "ora";
+import ora from 'ora'
 
-import chalk from "chalk";
+import chalk from 'chalk'
 
-import PrismaEngine from "prisma-cli-engine";
+// import PrismaEngine from 'prisma-cli-engine'
 
 
-import GenerateFragmentsHandler from "graphql-cli-generate-fragments/dist/GenerateFragments";
+import GenerateFragmentsHandler from 'graphql-cli-generate-fragments/dist/GenerateFragments'
 
 import PrismaYml from 'prisma-yml'
 
 
 // import prismaDeploy from "prisma-cli-core/dist/commands/deploy/deploy";
-import PrismaCliCore from "prisma-cli-core";
+import PrismaCliCore from 'prisma-cli-core'
 
-import GetSchema from "graphql-cli/dist/cmds/get-schema";
+import GetSchema from 'graphql-cli/dist/cmds/get-schema'
+// import addEndpoint from 'graphql-cli/dist/cmds/add-endpoint'
+import GraphqlConfig from 'graphql-config'
+
+console.log('GraphqlConfig', GraphqlConfig);
+
+const {
+  GraphQLEndpoint,
+} = GraphqlConfig;
 
 const {
   PrismaDefinitionClass,
-} = PrismaYml;
+} = PrismaYml
 
 const {
   handler,
-} = GetSchema;
+} = GetSchema
 
-const {
-  Config,
-} = PrismaEngine
+// const {
+//   Config,
+// } = PrismaEngine
 
 const {
   GenerateFragments,
-} = GenerateFragmentsHandler;
+} = GenerateFragmentsHandler
 
 const {
   Deploy: prismaDeploy,
-} = PrismaCliCore;
+} = PrismaCliCore
 
 
 class CustomGenerateFragments extends GenerateFragments {
-
   getProjectConfig() {
-
     return {
+      endpointsExtension: {
+        getEndpoint: (url) => {
+
+          console.log('endpointsExtension url', url);
+          const endpoint = new GraphQLEndpoint({
+            url,
+          });
+
+          return endpoint;
+        },
+      },
       app: {
         config: {
-          "extensions": {
-            "prepare-bundle": {
-              "output": "src/schema/generated/api.graphql"
+          extensions: {
+            'prepare-bundle': {
+              output: 'src/schema/generated/api.graphql'
             },
-            "generate-fragments": {
-              "generator": "js",
-              "output": "src/schema/generated/api.fragments.js"
+            'generate-fragments': {
+              generator: 'js',
+              output: 'src/schema/generated/api.fragments.js'
             },
           },
         },
@@ -59,22 +76,16 @@ class CustomGenerateFragments extends GenerateFragments {
   }
 
   async fragments() {
-
-    let result;
+    let result
 
     try {
-
       result = await super.fragments()
-    }
-    catch (error) {
-      this.context.spinner.fail(error.message);
+    } catch (error) {
+      this.context.spinner.fail(error.message)
     }
 
-    return result;
-
+    return result
   }
-
-
 }
 
 const generator = new CustomGenerateFragments({
@@ -98,89 +109,105 @@ const generator = new CustomGenerateFragments({
 
 
 const buildApiSchema = async function (generateSchema) {
-
-
   /**
    * Build api schema
    */
 
-  const apiSchema = generateSchema("api");
+  generateSchema('api')
 
 
-  await buildApiFragments();
-
-
+  await buildApiFragments()
 }
 
 
 const buildApiFragments = async function () {
-
-  const fragments = await generator.handle()
+  await generator.handle()
     .catch(error => {
-      console.error("Error", error);
-    });
-
+      console.error('Error', error)
+    })
 }
 
 
 
 
 const deploySchema = function (generateSchema) {
-
+  // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
-
-
-    let schema;
+    let schema
 
     try {
-      schema = generateSchema("prisma");
-    }
-    catch (error) {
-      console.error(chalk.red("deploySchema error"), error);
-      reject(error);
+      schema = generateSchema('prisma')
+    } catch (error) {
+      console.error(chalk.red('deploySchema error'), error)
+      reject(error)
     }
 
     if (!schema) {
-      return reject(new Error("Schema is empty"));
+      return reject(new Error('Schema is empty'))
     }
 
 
     class CustomPrismaDefinitionClass extends PrismaDefinitionClass {
 
+      // getClusterName() {
+
+      //   throw new Error ("getClusterName");
+
+      //   const name = super.getClusterName();
+
+      //   console.log(chalk.green('getClusterName name'), data)
+
+
+      //   return name;
+      // }
+
+      async getCluster(throws) {
+
+        const data = await super.getCluster(throws);
+
+        // console.log('getCluster data', data)
+
+        data.name = 'local'
+        return data;
+      }
+
+      async getClusterByEndpoint(data) {
+
+        const cluster = await super.getClusterByEndpoint(data)
+
+        // console.log('getClusterByEndpoint data', data)
+        // console.log('getClusterByEndpoint cluster', cluster)
+
+        // cluster.name = 'local'
+
+        return cluster
+      }
 
       async load(args, envPath) {
-
-
-        let {
+        const {
           envVars,
-          ...other
-        } = this;
+          // ...other
+        } = this
 
         const {
           endpoint,
-        } = envVars;
+        } = envVars
 
         this.definition = {
           endpoint,
           datamodel: true,
         }
 
-        this.typesString = schema;
-
-
-        return;
+        this.typesString = schema
       }
-
     }
 
 
     class Deploy extends prismaDeploy {
-
       constructor(options) {
+        super(options)
 
-        super(options);
-
-
+        console.log('this.env', this.env)
 
         this.definition = new CustomPrismaDefinitionClass(
           this.env,
@@ -188,95 +215,127 @@ const deploySchema = function (generateSchema) {
           process.env,
           this.out,
         )
-
       }
 
       // const spinner = ora('Deploy schema');
 
       async run(props, args) {
-
-        const spinner = ora('Deploy schema').start();
+        // const spinner = ora('Deploy schema').start()
 
 
         try {
-          const promise = super.run(props);
+          const promise = super.run(props)
 
+          let result
 
-          const result = await promise
+          // eslint-disable-next-line prefer-const
+          result = await promise
             .catch(error => {
-              spinner.fail(error.message);
-            });
+              console.error('error', error)
+              // spinner.fail(error.message)
+            })
 
-          const {
-            stderr,
-            stdout,
-          } = this.out;
+          // const {
+          //   stderr,
+          //   stdout,
+          // } = this.out
+
+          // const error = (stderr && stderr.output) || null
+
+          // if (error) {
+          //   console.error('stderr error', error)
+          //   // console.error('stderr', stderr)
+          // }
 
 
+          // if (stdout.output) {
+          //   // spinner.succeed(stdout.output)
+          // }
 
-          if (stdout.output) {
-            spinner.succeed(stdout.output);
-          }
+          resolve(result)
 
-          resolve(result);
-
-          return result;
+          return result
+        } catch (error) {
+          console.error(chalk.red('Error'), error)
+          reject(error)
         }
-        catch (error) {
-          console.error(chalk.red("Error"), error);
-          reject(error);
-        }
-
       }
-
-
     }
 
 
 
-    let argv = process.argv && process.argv.map(n => n) || [];
+    const argv = (process.argv && process.argv.map(n => n)) || []
 
-    const force = argv && argv.indexOf("--force") !== -1 || false;
+    const force = (argv && argv.indexOf('--force') !== -1) || false
 
     try {
-      const HandlerObject = await Deploy.mock(force ? "-f" : "")
+      await Deploy.mock(force ? '-f' : '')
         .then(handler => {
-
-          return handler;
+          return handler
         })
         .catch(error => {
-
-          console.error(chalk.red("Error 2"), error);
-        });
+          console.error(chalk.red('Error 2'), error)
+        })
 
       // console.log("HandlerObject.flags", HandlerObject.flags);
-
+    } catch (error) {
+      console.error(chalk.red('Error 2'), error)
     }
-    catch (error) {
-      console.error(chalk.red("Error 2"), error);
-    }
-
-  });
-
-
+  })
 }
 
+
+// const getSchema = async function () {
+//   const result = await handler({
+//     spinner: ora('Get schema'),
+//     getConfig: () => {
+
+//       return new CustomGenerateFragments()
+//     }
+//   }, {
+//     endpoint: process.env.endpoint,
+//     output: 'src/schema/generated/prisma.graphql',
+//   })
+//     .catch(error => {
+//       console.error(chalk.red('Get schema'), error)
+//     })
+
+//   return result
+// }
 
 const getSchema = async function () {
 
 
-
   const result = await handler({
     spinner: ora('Get schema'),
-  }, {
-      endpoint: process.env.endpoint,
-      output: "src/schema/generated/prisma.graphql",
-    })
-    .catch(error => {
-      console.error(chalk.red("Get schema"), error);
-    });
+    getConfig: () => {
 
-  return result;
+      const context = new CustomGenerateFragments({
+        // spinner: ora('Generate fragments'),
+        getConfig: (props) => {
+          return {
+            getProjectConfig: (props) => {
+              console.log('getProjectConfig', props)
+            },
+          }
+        },
+      }, {
+        project: 'app',
+        // generator: "js",
+      })
+
+      return context;
+      // return new CustomGenerateFragments()
+    }
+  }, {
+    endpoint: process.env.endpoint,
+    output: 'src/schema/generated/prisma.graphql',
+  })
+    .catch(error => {
+      console.error(chalk.red('Get schema'), error)
+    })
+
+  return result
 }
 
 export {
